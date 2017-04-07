778a5db65ba76e6f6d68e06bc16288bd6cedaab3
firefly0413 <akzhengfei11@163.com>
2017-04-07 21:57:17 +0800

    用户管理和分页

--- routers/admin.js
+++ routers/admin.js
@@ -2,6 +2,16 @@ var express = require("express");
 var router = express.Router();
 var User = require("../models/user");
 
+//统一返回格式
+var responseData;
+router.use(function(req,res,next){
+	responseData={
+		code:0,
+		message:""
+	};
+	next();
+});
+
 router.use(function(req,res,next){
 	if(!req.userInfo.isAdmin){
 		res.send("对不起，只有管理员才有权限进入管理页面！");
@@ -22,13 +32,31 @@ router.get("/",function(req,res,next){
 
 router.get("/users",function(req,res,next){
 
-	//从数据库中读取用户列表
-	User.find().then(function(users){
-		res.render("admin/user_manage",{
-			userInfo:req.userInfo,
-			users:users
+	var limit = 10;
+	var page = Number(req.query.page||1);
+	var total_page = 0;
+
+	User.count().then(function(count){
+		total_page = Math.ceil(count/limit);
+		//限制page大小
+		page = Math.min(page,total_page);
+		page = Math.max(page,1);
+
+		var skip = (page-1)*limit;
+
+		//从数据库中读取用户列表
+		User.find().limit(limit).skip(skip).then(function(users){
+			res.render("admin/user_manage",{
+				userInfo:req.userInfo,
+				users:users,
+				page:page,
+				total_page:total_page,
+				limit:limit,
+				count:count
+			})
 		})
 	})
+
 })
 
 module.exports = router;
